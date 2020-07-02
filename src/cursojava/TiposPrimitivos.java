8426f80b950b14da25cbfd0d948091acb8e80fdf
alexdeluna <alexdeluna2804@gmail.com>
2020-07-01 21:46:16 -0300

    primeiro commit

---
+++ src/cursojava/TiposPrimitivos.java
@@ -0,0 +1,15 @@
+package cursojava;
+
+public class TiposPrimitivos {
+
+		public static void main(String[] args) {
+			int a = 6;
+			int b = 2;
+			int c = 3;
+			int res;
+			res = a+b+c;
+			System.out.println("O resultado da soma é " + res);
+		
+	}
+
+}
