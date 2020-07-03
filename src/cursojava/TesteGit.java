738cd33f09bf55ad8aea1432a575f09de51e5690
alexdeluna <67716472+alexdeluna@users.noreply.github.com>
2020-07-03 11:20:33 -0300

    Criei a classe TesteGit

---
+++ src/cursojava/TesteGit.java
@@ -0,0 +1,15 @@
+package cursojava;
+
+import java.util.Scanner;
+
+public class TesteGit {
+
+	public static void main(String[] args) {
+		String nome;
+		Scanner in = new Scanner(System.in);
+		System.out.println("DIGITE SEU NOME! ");
+		nome = in.next();
+		System.out.println(nome);
+	}
+
+}
